Added differentiation wrt mesh coordinates directly, which fixes broken derivatives when deformations are large

femo_alpha/csdl_alpha_opt/output_operation.py
@@ -10,6 +10,40 @@ from femo_alpha.fea.fea_dolfinx import (
 import csdl_alpha as csdl
 import numpy as np
 from ufl import TestFunction, TrialFunction, inner, dx
+import ufl
+from dolfinx.fem import Function
+
+
+def _is_mesh_coordinates(arg):
+    return arg.get("type", "function") == "mesh_coordinates"
+
+
+def _update_input(arg, values):
+    if not _is_mesh_coordinates(arg):
+        update(arg["function"], values)
+        return
+    values = np.asarray(values).reshape(arg["shape"])
+    local_values = values[arg["input_global_indices"]]
+    if local_values.shape != arg["mesh"].geometry.x.shape:
+        raise ValueError(
+            f"Mesh coordinates map to {local_values.shape}; "
+            f"expected {arg['mesh'].geometry.x.shape}."
+        )
+    arg["mesh"].geometry.x[:] = local_values
+
+
+def _coordinate_function(arg, external_values):
+    direction = Function(arg["function_space"])
+    values = np.asarray(external_values).reshape(arg["shape"])
+    update(direction, values[arg["input_global_indices"]].reshape(-1))
+    return direction
+
+
+def _external_coordinate_values(arg, local_values):
+    local_values = np.asarray(local_values).reshape((-1, arg["mesh"].geometry.dim))
+    external_values = np.zeros(arg["shape"], dtype=local_values.dtype)
+    np.add.at(external_values, arg["input_global_indices"], local_values)
+    return external_values
 
 
 class OutputOperation(csdl.CustomExplicitOperation):
@@ -60,19 +94,31 @@ class OutputOperation(csdl.CustomExplicitOperation):
     def compute(self, input_vals, output_vals):
         for arg_name in input_vals:
             arg = self.args_dict[arg_name]
-            update(arg["function"], input_vals[arg_name])
+            _update_input(arg, input_vals[arg_name])
 
         output_vals[self.output_name] = assemble(self.fea_output["form"])
 
     def compute_derivatives(self, input_vals, output_vals, derivatives):
         for arg_name in input_vals:
             arg = self.args_dict[arg_name]
-            update(arg["function"], input_vals[arg_name])
+            _update_input(arg, input_vals[arg_name])
 
         for arg_name in input_vals:
+            arg = self.args_dict[arg_name]
+            if _is_mesh_coordinates(arg):
+                coordinate_test = TestFunction(arg["function_space"])
+                coordinate_derivative = ufl.derivative(
+                    self.fea_output["form"],
+                    arg["coordinate"],
+                    coordinate_test,
+                )
+                local_gradient = assemble(coordinate_derivative, dim=1)
+                external_gradient = _external_coordinate_values(arg, local_gradient)
+                derivatives[self.output_name, arg_name] = external_gradient.reshape((1, -1))
+                continue
             derivatives[self.output_name, arg_name] = assemble(
                 computePartials(
-                    self.fea_output["form"], self.args_dict[arg_name]["function"]
+                    self.fea_output["form"], arg["function"]
                 ),
                 dim=self.output_dim + 1,
             )
@@ -109,6 +155,7 @@ class OutputFieldOperation(csdl.CustomExplicitOperation):
         # Lazily initialized projection operators used by jacobian-vector products.
         self._projection_rhs_form = None
         self._projection_mass_matrix = None
+        self._projection_mass_form = None
         self._projection_ksp = None
 
     def evaluate(self, inputs: csdl.VariableGroup):
@@ -130,7 +177,7 @@ class OutputFieldOperation(csdl.CustomExplicitOperation):
     def compute(self, input_vals, output_vals):
         for arg_name in input_vals:
             arg = self.args_dict[arg_name]
-            update(arg['function'], input_vals[arg_name])
+            _update_input(arg, input_vals[arg_name])
         self.fea.projectFieldOutput(self.fea_output['form'],self.fea_output['function'])
         output_vals[self.output_name] = getFuncArray(self.fea_output['function'])
 
@@ -142,9 +189,6 @@ class OutputFieldOperation(csdl.CustomExplicitOperation):
             )
 
     def _initialize_projection_operators(self):
-        if self._projection_mass_matrix is not None:
-            return
-
         output_space = self.fea_output['function'].function_space
         test_func = TestFunction(output_space)
         trial_func = TrialFunction(output_space)
@@ -152,14 +196,15 @@ class OutputFieldOperation(csdl.CustomExplicitOperation):
         # Projection equation: M * y = b where
         # b_i = int(inner(v(x), w_i) dx)
         self._projection_rhs_form = inner(self.fea_output['form'], test_func) * dx
-        mass_form = inner(trial_func, test_func) * dx
-        self._projection_mass_matrix = assembleMatrix(mass_form)
+        self._projection_mass_form = inner(trial_func, test_func) * dx
+        self._projection_mass_matrix = assembleMatrix(self._projection_mass_form)
         self._projection_ksp = setUpKSP_MUMPS(self._projection_mass_matrix)
 
     def compute_jacvec_product(self, input_vals, output_vals, d_inputs, d_outputs, mode):
         for arg_name in input_vals:
             arg = self.args_dict[arg_name]
-            update(arg['function'], input_vals[arg_name])
+            _update_input(arg, input_vals[arg_name])
+        update(self.fea_output["function"], output_vals[self.output_name])
 
         if self.output_name not in d_outputs:
             return
@@ -175,9 +220,31 @@ class OutputFieldOperation(csdl.CustomExplicitOperation):
                     d_arg = d_inputs[arg_name]
                     if d_arg is None:
                         continue
+                    arg = self.args_dict[arg_name]
+                    if _is_mesh_coordinates(arg):
+                        direction = _coordinate_function(arg, d_arg)
+                        d_rhs_form = ufl.derivative(
+                            self._projection_rhs_form,
+                            arg["coordinate"],
+                            direction,
+                        )
+                        rhs.array[:] += assemble(d_rhs_form, dim=1)
+                        d_mass_form = ufl.derivative(
+                            self._projection_mass_form,
+                            arg["coordinate"],
+                            direction,
+                        )
+                        d_mass_matrix = assembleMatrix(d_mass_form)
+                        current_output_vec = d_mass_matrix.createVecRight()
+                        current_output_vec.array[:] = output_vals[self.output_name].reshape(-1)
+                        current_output_vec.assemble()
+                        mass_contrib = d_mass_matrix.createVecLeft()
+                        d_mass_matrix.mult(current_output_vec, mass_contrib)
+                        rhs.array[:] -= mass_contrib.getArray()
+                        continue
                     d_rhs_form = computePartials(
                         self._projection_rhs_form,
-                        self.args_dict[arg_name]['function'],
+                        arg['function'],
                     )
                     d_rhs_mat = assembleMatrix(d_rhs_form)
 
@@ -206,6 +273,33 @@ class OutputFieldOperation(csdl.CustomExplicitOperation):
 
             for arg_name in self.args_dict:
                 if arg_name in d_inputs:
+                    arg = self.args_dict[arg_name]
+                    if _is_mesh_coordinates(arg):
+                        coordinate_trial = TrialFunction(arg["function_space"])
+                        d_rhs_form = ufl.derivative(
+                            self._projection_rhs_form,
+                            arg["coordinate"],
+                            coordinate_trial,
+                        )
+                        d_rhs_matrix = assembleMatrix(d_rhs_form)
+                        mass_action = ufl.action(
+                            self._projection_mass_form,
+                            self.fea_output["function"],
+                        )
+                        d_mass_action = ufl.derivative(
+                            mass_action,
+                            arg["coordinate"],
+                            coordinate_trial,
+                        )
+                        d_mass_matrix = assembleMatrix(d_mass_action)
+                        local_contrib = d_rhs_matrix.createVecRight()
+                        d_rhs_matrix.multTranspose(adjoint_rhs, local_contrib)
+                        mass_contrib = d_mass_matrix.createVecRight()
+                        d_mass_matrix.multTranspose(adjoint_rhs, mass_contrib)
+                        local_contrib.array[:] -= mass_contrib.getArray()
+                        external = _external_coordinate_values(arg, local_contrib.getArray())
+                        d_inputs[arg_name] += external
+                        continue
                     d_rhs_form = computePartials(
                         self._projection_rhs_form,
                         self.args_dict[arg_name]['function'],

femo_alpha/csdl_alpha_opt/state_operation.py
@@ -4,10 +4,16 @@ from femo_alpha.fea.fea_dolfinx import (FEA, update, getFuncArray, computePartia
                                   assembleSystem, setUpKSP_MUMPS)
 import csdl_alpha as csdl
 import numpy as np
+import ufl
+from dolfinx.fem import Function
 
 
 def is_direct_vector_input(arg):
     return arg.get("type", "function") in ("vector", "direct_vector")
+
+
+def is_mesh_coordinates_input(arg):
+    return arg.get("type", "function") == "mesh_coordinates"
 
 
 class StateOperation(csdl.experimental.CustomImplicitOperation):
@@ -107,6 +113,9 @@ class StateOperation(csdl.experimental.CustomImplicitOperation):
         direct_residual_inputs = []
         for arg_name in input_vals:
             arg = self.args_dict[arg_name]
+            if is_mesh_coordinates_input(arg):
+                self._update_mesh_coordinates(arg_name, arg, input_vals[arg_name])
+                continue
             if is_direct_vector_input(arg):
                 direct_residual_inputs.append(
                     self._build_direct_residual_input(arg_name, input_vals[arg_name])
@@ -198,10 +207,21 @@ class StateOperation(csdl.experimental.CustomImplicitOperation):
                         d_residuals[state_name] += data.get('sign', -1.0) * contrib
                         continue
 
-                    update(data['fea_df'], d_inputs[arg_name])
-                    dRdf = data['dRdf']
+                    if data["kind"] == "mesh_coordinates":
+                        values = np.asarray(d_inputs[arg_name]).reshape(data["shape"])
+                        local_values = values[data["input_global_indices"]].reshape(-1)
+                        update(data["fea_df"], local_values)
+                        contrib = computeMatVecProductFwd(data["dRdf"], data["fea_df"])
+                        contrib = self._zero_constrained_entries(
+                            contrib, data.get("constrained_dofs")
+                        )
+                        d_residuals[state_name] += contrib
+                        continue
+
+                    update(data["fea_df"], d_inputs[arg_name])
+                    dRdf = data["dRdf"]
                     d_residuals[state_name] += computeMatVecProductFwd(
-                            dRdf, data['fea_df'])
+                            dRdf, data["fea_df"])
         # for mode = rev
         # d_residuals --> d_inputs
         elif mode == 'rev':
@@ -230,9 +250,21 @@ class StateOperation(csdl.experimental.CustomImplicitOperation):
                         )
                         continue
 
-                    dRdf = data['dRdf']
+                    dRdf = data["dRdf"]
                     d_input_contrib = computeMatVecProductBwd(
                         dRdf, self.fea_dR)
+                    if data["kind"] == "mesh_coordinates":
+                        local_values = d_input_contrib.reshape((-1, data["gdim"]))
+                        external_values = np.zeros(data["shape"], dtype=local_values.dtype)
+                        np.add.at(
+                            external_values,
+                            data["input_global_indices"],
+                            local_values,
+                        )
+                        d_inputs[arg_name] += external_values.reshape(
+                            d_inputs[arg_name].shape
+                        )
+                        continue
                     d_inputs[arg_name] += d_input_contrib.reshape(
                         d_inputs[arg_name].shape)
         else:
@@ -299,6 +331,24 @@ class StateOperation(csdl.experimental.CustomImplicitOperation):
         for arg_ind in range(len(arg_list)):
             arg_name = arg_list[arg_ind]
             arg = self.args_dict[arg_name]
+            if is_mesh_coordinates_input(arg):
+                coordinate_trial = ufl.TrialFunction(arg["function_space"])
+                dR_df = ufl.derivative(
+                    self.fea_state["residual_form"],
+                    arg["coordinate"],
+                    coordinate_trial,
+                )
+                constrained_dofs = self._get_default_constrained_dofs()
+                dR_df_dict[arg_name] = dict(
+                    kind="mesh_coordinates",
+                    dR_df=dR_df,
+                    fea_df=Function(arg["function_space"]),
+                    shape=arg["shape"],
+                    gdim=arg["mesh"].geometry.dim,
+                    input_global_indices=arg["input_global_indices"],
+                    constrained_dofs=constrained_dofs,
+                )
+                continue
             if is_direct_vector_input(arg):
                 constrained_dofs = arg.get('constrained_dofs')
                 if constrained_dofs is None:
@@ -347,9 +397,12 @@ class StateOperation(csdl.experimental.CustomImplicitOperation):
         # update the input values in the FEA model
         for arg_name in input_vals:
             arg = self.args_dict[arg_name]
+            if is_mesh_coordinates_input(arg):
+                self._update_mesh_coordinates(arg_name, arg, input_vals[arg_name])
+                continue
             if is_direct_vector_input(arg):
                 continue
-            update(arg['function'], input_vals[arg_name])
+            update(arg["function"], input_vals[arg_name])
         update(self.fea_state['function'], output_vals[self.state_name])
 
         dR_df_dict = self.dR_df_dict
@@ -369,6 +422,22 @@ class StateOperation(csdl.experimental.CustomImplicitOperation):
                         bcs=self.fea.bc)
 
         self.ksp = setUpKSP_MUMPS(self.A)
+
+    def _update_mesh_coordinates(self, arg_name, arg, values):
+        values = np.asarray(values)
+        if values.size != int(np.prod(arg["shape"])):
+            raise ValueError(
+                f"Mesh-coordinate input {arg_name!r} has size {values.size}, "
+                f"expected {int(np.prod(arg['shape']))}."
+            )
+        values = values.reshape(arg["shape"])
+        local_values = values[arg["input_global_indices"]]
+        if local_values.shape != arg["mesh"].geometry.x.shape:
+            raise ValueError(
+                f"Mesh-coordinate input {arg_name!r} maps to shape "
+                f"{local_values.shape}, expected {arg['mesh'].geometry.x.shape}."
+            )
+        arg["mesh"].geometry.x[:] = local_values
 
     def _get_default_constrained_dofs(self):
         if self._default_constrained_dofs is not None:

femo_alpha/rm_shell/rm_shell_pde.py
@@ -15,7 +15,6 @@ from femo_alpha.rm_shell.linear_shell_fenicsx.linear_shell_model import (ShellEl
                                                                     MaterialModel,
                                                                     ElasticModelShapeOpt)
 from femo_alpha.rm_shell.linear_shell_fenicsx.utils import computeNodalDisp
-from femo_alpha.rm_shell.linear_shell_fenicsx.kinematics import J
 
 
 class RMShellPDE:
@@ -42,15 +41,14 @@ class RMShellPDE:
             self.VF = VectorFunctionSpace(mesh, ("DG", 0))
         else:
             self.VF = VectorFunctionSpace(mesh, ("CG", 1))
-        self.VU = VectorFunctionSpace(mesh, ("CG", 1))
         self.bf_sup_sizes = assemble_vector(
                 form(TestFunction(self.VF.sub(0).collapse()[0])*dx)).getArray()
         # self.bf_sup_sizes = np.ones_like(self.bf_sup_sizes)
 
-    def pdeRes(self, h, w, uhat, f, m, E, nu, penalty=False, dss=ufl.ds, dSS=ufl.dS, g=None):
+    def pdeRes(self, h, w, f, m, E, nu, penalty=False, dss=ufl.ds, dSS=ufl.dS, g=None):
         material_model = MaterialModel(E=E,nu=nu,h=h)
         self.elastic_model = elastic_model = ElasticModelShapeOpt(self.mesh,
-                                                w, uhat, material_model.CLT)
+                                                w, material_model.CLT)
         elastic_energy = elastic_model.elasticEnergy(E, h,
                                     self.dx_inplane,self.dx_shear)
         res = elastic_model.weakFormResidual(elastic_energy, f=f, m=m,
@@ -58,18 +56,18 @@ class RMShellPDE:
         return res
         # # Inertia relief
         # f_d = ufl.as_vector([0.,0.,-rho*h*g])
-        # res -= inner(f_d,elastic_model.du_mid)*J(uhat)*dx
+        # res -= inner(f_d,elastic_model.du_mid)*dx
         # return res
 
-    def assemble_generalized_load_vector(self, uhat, f=None, m=None):
+    def assemble_generalized_load_vector(self, f=None, m=None):
         dw = TestFunction(self.W)
         du_mid, dtheta = ufl.split(dw)
 
         load_form = 0
         if f is not None:
-            load_form += inner(f, du_mid) * J(uhat) * dx
+            load_form += inner(f, du_mid) * dx
         if m is not None:
-            load_form += inner(m, dtheta) * J(uhat) * dx
+            load_form += inner(m, dtheta) * dx
         if f is None and m is None:
             raise ValueError("At least one of f or m must be provided.")
 
@@ -96,28 +94,28 @@ class RMShellPDE:
         # No regularization
         return regularization
 
-    def compliance(self, u_mid, theta, uhat, h, f, m):
-        compliance = inner(u_mid, f) * J(uhat) * ufl.dx
-        compliance += inner(theta, m) * J(uhat) * ufl.dx
+    def compliance(self, u_mid, theta, h, f, m):
+        compliance = inner(u_mid, f) * ufl.dx
+        compliance += inner(theta, m) * ufl.dx
         return compliance
     
-    def tip_disp(self,u_mid,uhat,dxx):
-        return Constant(self.mesh, 0.5)*inner(u_mid,u_mid)*J(uhat)*dxx
+    def tip_disp(self,u_mid,dxx):
+        return Constant(self.mesh, 0.5)*inner(u_mid,u_mid)*dxx
     
-    def volume(self,uhat,h):
-        return h*J(uhat)*dx
+    def volume(self,h):
+        return h*dx
 
-    def mass(self,uhat,h,rho):
-        return rho*h*J(uhat)*dx
+    def mass(self,h,rho):
+        return rho*h*dx
     
-    def cg_form(self, uhat, h, rho):
+    def cg_form(self, h, rho):
         """
         Compute the center of gravity forms.
         Returns forms for CG_x, CG_y, CG_z numerators and total mass.
         CG = sum(position * mass) / sum(mass)
         """
-        x = ufl.SpatialCoordinate(self.mesh)
-        mass_element = rho * h * J(uhat)
+        x = self.mesh._femo_spatial_coordinate
+        mass_element = rho * h
         
         cg_x_numerator = x[0] * mass_element * dx
         cg_y_numerator = x[1] * mass_element * dx
@@ -126,40 +124,40 @@ class RMShellPDE:
         
         return cg_x_numerator, cg_y_numerator, cg_z_numerator, total_mass
     
-    def area_subdomain(self,uhat,dxx):
-        return J(uhat)*dxx
+    def area_subdomain(self,dxx):
+        return Constant(self.mesh, 1.0) * dxx
     
-    def elastic_energy(self,w,uhat,h,E):
+    def elastic_energy(self,w,h,E):
         elastic_energy = self.elastic_model.elasticEnergy(E, h, 
                                         self.dx_inplane, self.dx_shear)
         return elastic_energy
 
-    def pnorm_stress(self,w,uhat,h,E,nu,dx,m=1e-6,rho=100,alpha=None,regularization=False):
+    def pnorm_stress(self,w,h,E,nu,dx,m=1e-6,rho=100,alpha=None,regularization=False):
         """
         Compute the p-norm of the stress
         `rho` is the Constraint aggregation factor
         """
-        shell_stress_RM = ShellStressRM(self.mesh, w, uhat, h, E, nu)
+        shell_stress_RM = ShellStressRM(self.mesh, w, h, E, nu)
         # stress on the top surface
         vm_stress = shell_stress_RM.vonMisesStress(h/2)
-        pnorm = (m*vm_stress)**rho*J(uhat)*dx
+        pnorm = (m*vm_stress)**rho*dx
         if regularization:
-            regularization = 0.5*Constant(self.mesh, 1e3)*h**rho*J(uhat)*dx
+            regularization = 0.5*Constant(self.mesh, 1e3)*h**rho*dx
             pnorm += regularization
         if alpha == None:
             ##### alpha is a parameter based on the surface area
-            alpha_form = Constant(self.mesh,1.0)*J(uhat)*dx
+            alpha_form = Constant(self.mesh,1.0)*dx
             alpha = assemble_scalar(form(alpha_form))
         return 1/alpha*pnorm
     
-    def sum_stress_subdomain(self,w,uhat,h,E,nu,dxx):
+    def sum_stress_subdomain(self,w,h,E,nu,dxx):
         """
         Computes the average stress in global coordiantes over a subdomain
         """
-        shell_stress_RM = ShellStressRM(self.mesh, w, uhat, h, E, nu)
+        shell_stress_RM = ShellStressRM(self.mesh, w, h, E, nu)
         # stress on the top surface
         # vm_stress = shell_stress_RM.vonMisesStress(h/2)
-        # sum_subdomain = vm_stress*J(uhat)*dxx
+        # sum_subdomain = vm_stress*dxx
         sigma = shell_stress_RM.inplaneStress(h/2)
         sigma_x = sigma[0,0]
         sigma_y = sigma[1,1]
@@ -167,16 +165,16 @@ class RMShellPDE:
         sigma_xy = sigma[0,1]
         sigma_xz = sigma[0,2]
         sigma_yz = sigma[1,2]
-        sum_x = sigma_x*J(uhat)*dxx
-        sum_y = sigma_y*J(uhat)*dxx
-        sum_z = sigma_z*J(uhat)*dxx
-        sum_xy = sigma_xy*J(uhat)*dxx
-        sum_xz = sigma_xz*J(uhat)*dxx
-        sum_yz = sigma_yz*J(uhat)*dxx
+        sum_x = sigma_x*dxx
+        sum_y = sigma_y*dxx
+        sum_z = sigma_z*dxx
+        sum_xy = sigma_xy*dxx
+        sum_xz = sigma_xz*dxx
+        sum_yz = sigma_yz*dxx
         return sum_x, sum_y, sum_z, sum_xy, sum_xz, sum_yz
 
-    def von_Mises_stress(self,w,uhat,h,E,nu,surface='Top'):
-        shell_stress_RM = ShellStressRM(self.mesh, w, uhat, h, E, nu)
+    def von_Mises_stress(self,w,h,E,nu,surface='Top'):
+        shell_stress_RM = ShellStressRM(self.mesh, w, h, E, nu)
         if surface == 'Top':
             # stress on the top surface
             vm_stress = shell_stress_RM.vonMisesStress(h/2)

tests/test_rmshell_direct_load_vector.py
@@ -44,7 +44,7 @@ def _build_surface_mesh(nx=4, ny=2):
         return xdmf.read_mesh(name="Grid")
 
 
-def _build_shell_model(recorder_path, nx=4, ny=2, record=True):
+def _build_shell_model(recorder_path, nx=4, ny=2, record=True, geometry_input_mode="coordinates"):
     mesh = _build_surface_mesh(nx=nx, ny=ny)
     return RMShellModel(
         mesh,
@@ -53,6 +53,7 @@ def _build_shell_model(recorder_path, nx=4, ny=2, record=True):
         record=record,
         solve_direct=True,
         recorder_path=recorder_path,
+        geometry_input_mode=geometry_input_mode,
     )
 
 
@@ -103,12 +104,9 @@ def _fenics_ordered_inputs_with_moments(shell_model):
 def _assemble_load_direction(shell_model, field_direction):
     f_func = Function(shell_model.shell_pde.VF)
     m_func = Function(shell_model.shell_pde.VF)
-    uhat_func = Function(shell_model.shell_pde.VU)
     f_func.x.array[:] = field_direction
     m_func.x.array[:] = 0.0
-    uhat_func.x.array[:] = 0.0
     return shell_model.shell_pde.assemble_generalized_load_vector(
-        uhat=uhat_func,
         f=f_func,
         m=m_func,
     )
@@ -117,12 +115,9 @@ def _assemble_load_direction(shell_model, field_direction):
 def _assemble_combined_load_direction(shell_model, force_direction, moment_direction):
     f_func = Function(shell_model.shell_pde.VF)
     m_func = Function(shell_model.shell_pde.VF)
-    uhat_func = Function(shell_model.shell_pde.VU)
     f_func.x.array[:] = force_direction
     m_func.x.array[:] = moment_direction
-    uhat_func.x.array[:] = 0.0
     return shell_model.shell_pde.assemble_generalized_load_vector(
-        uhat=uhat_func,
         f=f_func,
         m=m_func,
     )
@@ -436,6 +431,13 @@ def test_rmshell_compliance_matches_generalized_load_work():
         atol=1e-10,
     )
 
+    np.testing.assert_allclose(
+        2.0 * outputs.elastic_energy.value,
+        outputs.compliance.value,
+        rtol=1e-8,
+        atol=1e-8,
+    )
+
     recorder.stop()
 
 
@@ -633,5 +635,104 @@ def test_rmshell_post_registry_supports_groups_and_custom_outputs():
         rtol=1e-10,
         atol=1e-10,
     )
+
+    recorder.stop()
+
+
+def test_rmshell_accepts_direct_mesh_coordinates_and_differentiates_them():
+    shell_model = _build_shell_model(
+        str(RECORDS_ROOT / "rmshell_direct_mesh_coordinates"),
+        nx=2,
+        ny=1,
+        record=False,
+        geometry_input_mode="coordinates",
+    )
+    vals = _fenics_ordered_inputs(shell_model)
+
+    recorder = csdl.Recorder(inline=True)
+    recorder.start()
+
+    material = _build_isotropic_material(shell_model, vals, "direct_coordinates")
+    loads = _build_vector_loads(shell_model, vals, "direct_coordinates")
+    mesh_nodes = csdl.Variable(
+        value=shell_model.reference_mesh_nodes.copy(),
+        name="direct_mesh_coordinates",
+    )
+    state = shell_model.solve(
+        material,
+        loads,
+        mesh_nodes=mesh_nodes,
+    )
+    outputs = shell_model.post.evaluate(state=state)
+
+    assert state.mesh_nodes is mesh_nodes
+    assert state.node_disp is None
+    np.testing.assert_allclose(state.raw_outputs.disp_solid.value, outputs.disp_solid.value)
+
+    derivative = csdl.derivative(outputs.compliance, [mesh_nodes])[mesh_nodes]
+    assert derivative.shape == (1, int(np.prod(mesh_nodes.shape)))
+    assert np.all(np.isfinite(derivative.value))
+    assert np.linalg.norm(derivative.value) > 0.0
+
+    recorder.stop()
+
+
+def test_rmshell_mesh_coordinate_state_and_post_derivatives_match_finite_difference():
+    shell_model = _build_shell_model(
+        str(RECORDS_ROOT / "rmshell_mesh_coordinate_fd"),
+        nx=2,
+        ny=1,
+        record=False,
+    )
+    vals = _fenics_ordered_inputs(shell_model)
+
+    recorder = csdl.Recorder(inline=True)
+    recorder.start()
+
+    material = _build_isotropic_material(shell_model, vals, "coordinate_fd")
+    loads = _build_vector_loads(shell_model, vals, "coordinate_fd")
+
+    reference = shell_model.reference_mesh_nodes.copy()
+    direction = np.zeros_like(reference)
+    direction[:, 0] = 0.03 * reference[:, 0] / np.max(reference[:, 0])
+    direction[:, 1] = 0.02 * reference[:, 1] / np.max(reference[:, 1])
+    direction[:, 2] = (
+        0.01
+        * reference[:, 0]
+        / np.max(reference[:, 0])
+        * (reference[:, 1] - np.mean(reference[:, 1]))
+    )
+    geometry_control = csdl.Variable(value=np.array([0.0]), name="geometry_control")
+    mesh_nodes = (
+        csdl.Variable(value=reference, name="reference_mesh_nodes")
+        + geometry_control * csdl.Variable(value=direction, name="mesh_direction")
+    )
+
+    state = shell_model.solve(material, loads, mesh_nodes=mesh_nodes)
+    shell_model.post.compliance().mass().cg().elastic_energy().mid_strain().shear_strain().curvature()
+    outputs = shell_model.post.compute(state=state)
+
+    checked_outputs = [
+        outputs.compliance,
+        outputs.mass,
+        outputs.cg[0],
+        outputs.cg[1],
+        outputs.elastic_energy,
+        csdl.sum(outputs.mid_strain),
+        csdl.sum(outputs.shear_strain),
+        csdl.sum(outputs.curvature),
+    ]
+    sim = csdl.experimental.PySimulator(recorder)
+    totals = sim.check_totals(
+        checked_outputs,
+        [geometry_control],
+        step_size=2e-6,
+        print_results=False,
+        raise_on_error=True,
+    )
+
+    for result in totals.values():
+        assert np.all(np.isfinite(result["value"]))
+        assert np.all(np.isfinite(result["fd_value"]))
 
     recorder.stop()